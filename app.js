ea146fc1cda0844b9a23849bf3e8bb31e02fd795
KorAidenChoi <64228107+KorAidenChoi@users.noreply.github.com>
2021-08-11 19:41:33 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+var canvas = document.createElement('canvas');
+//Constants
+var CANVAS_WIDTH = 800;
+var CANVAS_HEIGHT = 600;
+var NANONAUT_WIDTH = 181;
+var NANONAUT_HEIGHT = 229;
+var GROUD_Y = 540;
+//Setup
+
+//Main Loop
+
+//Player imput
+
+//Updating(Control Section)
+
+//Graphics(Drawing)
